e9a76c02d57878da6e4b6f9a7229e0b26f897341
Swifty-Sousa <chso8299@colorado.edu>
2020-02-06 08:04:36 -0700

    keylenght found, implemeitng last stage

--- p1/vigenere.py
+++ p1/vigenere.py
@@ -7,7 +7,7 @@ key1= "yz"
 key2= "xyz"
 key3= "wxyz"
 key4= "vwxyz"
-key5= "uvwxyz"
+key5= "AsvwBsgyCwdcs"
 #taken from Wikipedia
 letter_freqs = {
     'A': 0.08167,
@@ -50,6 +50,7 @@ def pop_var(s):
 #this function used for the test exersies in the assignment
 #this fuction based of off thise encryptor https://gist.github.com/dssstr/aedbb5e9f2185f366c6d6b50fad3e4a4
 def encrypt(pt, k):
+    k=k.upper()
     k_len = len(k)
     k_int = [ord(i) for i in k]
     pt_int= [ord(i) for i in pt]
@@ -63,6 +64,7 @@ def encrypt(pt, k):
 #this function used for the test exersies in the assignment
 #this fuction based of off thise encryptor https://gist.github.com/dssstr/aedbb5e9f2185f366c6d6b50fad3e4a4
 def decrypt(ct, k):
+    k= k.upper()
     k_len = len(k)
     k_int = [ord(i) for i in k]
     ct_int= [ord(i)for i in ct]
@@ -73,25 +75,26 @@ def decrypt(ct, k):
     return pt
 
 def encrypt_varance(pt, key):
+    key=key.upper()
     cipher_text= encrypt(pt,key)
     ct_var= pop_var(cipher_text) 
     print("The variance with key ", key, "is", ct_var)
     print("")
 
-def find_len(ct, leng):
-    new_cipher= ct[::leng]
-    new_cipher_var= pop_var(new_cipher)
-    nc_even= new_cipher[::2]
-    nc_odd= new_cipher[1::2]
-    even_var= pop_var(nc_even)
-    odd_var= pop_var(nc_odd)
-    print(stats.mean([even_var, odd_var]))
-    return(stats.mean([even_var, odd_var]))
+def find_len(ct):
+    variances=[]
+    for i in range(2,14):
+        variances.append(find_len_helper(ct,i)) 
+    print(*variances, sep='\n')
+    for i in variances:
+            if i>=0.001:
+                #we add two because the list starts with a key lenth of two, therefore a key of length 6 will have index 4 in the list
+                return (variances.index(i)+2)
 
-def find_len2(ct, leng):
+def find_len_helper(ct, leng):
     holder=[]
-    for i in range(2, leng):
-        new_cipher=ct[::i]
+    for i in range(0, leng-1):
+        new_cipher=ct[i::leng]
         new_cipher_var=pop_var(new_cipher) 
         holder.append(new_cipher_var)
     return(stats.mean(holder))
@@ -99,21 +102,26 @@ def find_len2(ct, leng):
 if __name__ == "__main__":
     # Read ciphertext from stdin
     # Ignore line breaks and spaces, convert to all upper case
+    #cipher = sys.stdin.read().replace("\n", "").replace(" ", "").upper()
+    sample_plaintext= "ethicslawanduniversitypolicieswarningtodefendasystemyouneedtobeabletothinklikeanattackerandthatincludesunderstandingtechniquesthatcanbeusedtocompromisesecurityhoweverusingthosetechniquesintherealworldmayviolatethelawortheuniversitysrulesanditmaybeunethicalundersomecircumstancesevenprobingforweaknessesmayresultinseverepenaltiesuptoandincludingexpulsioncivilfinesandjailtimeourpolicyineecsisthatyoumustrespecttheprivacyandpropertyrightsofothersatalltimesorelseyouwillfailthecourseactinglawfullyandethicallyisyourresponsibilitycarefullyreadthecomputerfraudandabuseactcfaaafederalstatutethatbroadlycriminalizescomputerintrusionthisisoneofseverallawsthatgovernhackingunderstandwhatthelawprohibitsifindoubtwecanreferyoutoanattorneypleasereviewitsspoliciesonresponsibleuseoftechnologyresourcesandcaenspolicydocumentsforguidelinesconcerningproper"
+    sample_plaintext= sample_plaintext.upper()
+    cipher= encrypt(sample_plaintext, key5)
+    key_len= find_len(cipher)
+    print("the key length is: ", key_len)
 
-    cipher = sys.stdin.read().replace("\n", "").replace(" ", "").upper()
+
     #################################################################
     # first calculate the population variance in the given relitive frequenceies above
     erf= stats.variance(letter_freqs.values()) #English relitive frequncy 
-    print("The population variance of the relitive english words is: ", erf)
-    print("")
+    #print("The population variance of the relitive english words is: ", erf)
+    #print("")
 
     #Sample plaintext:
-    sample_plaintext= "ethicslawanduniversitypolicieswarningtodefendasystemyouneedtobeabletothinklikeanattackerandthatincludesunderstandingtechniquesthatcanbeusedtocompromisesecurityhoweverusingthosetechniquesintherealworldmayviolatethelawortheuniversitysrulesanditmaybeunethicalundersomecircumstancesevenprobingforweaknessesmayresultinseverepenaltiesuptoandincludingexpulsioncivilfinesandjailtimeourpolicyineecsisthatyoumustrespecttheprivacyandpropertyrightsofothersatalltimesorelseyouwillfailthecourseactinglawfullyandethicallyisyourresponsibilitycarefullyreadthecomputerfraudandabuseactcfaaafederalstatutethatbroadlycriminalizescomputerintrusionthisisoneofseverallawsthatgovernhackingunderstandwhatthelawprohibitsifindoubtwecanreferyoutoanattorneypleasereviewitsspoliciesonresponsibleuseoftechnologyresourcesandcaenspolicydocumentsforguidelinesconcerningproper"
-    sample_plaintext= sample_plaintext.upper()
+
     
     # population variance of the sample plaintext
-    sample_plaintext_variance= pop_var(sample_plaintext)
-    print("The population variance of the relative words in the sample plaintext is: ",sample_plaintext_variance)
+    #sample_plaintext_variance= pop_var(sample_plaintext)
+    #print("The population variance of the relative words in the sample plaintext is: ",sample_plaintext_variance)
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     #testing the encryptor and decryptor:
@@ -127,12 +135,14 @@ if __name__ == "__main__":
     #encrypt_varance(sample_plaintext, key2)
     #encrypt_varance(sample_plaintext, key3)
     #encrypt_varance(sample_plaintext, key4)
-    encrypt_varance(sample_plaintext, key5)
+    #encrypt_varance(sample_plaintext, key5)
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     #Part4
-    #cipher_text= encrypt(sample_plaintext, key1)
-    #cipher_text_even= cipher_text[::2]
+    #cipher_text= encrypt(sample_plaintext, key5)
+    #cipher_text_even= cipher_text[0::2]
     #cipher_text_odd= cipher_text[1::2]
+    #cipher_text_three= cipher_text[::3]
+    #three_var=pop_var(cipher_text_three)
     #even_var= pop_var(cipher_text_even)
     #odd_var= pop_var(cipher_text_odd)
     #print("the even variance with key yz is: ", even_var)
@@ -140,9 +150,9 @@ if __name__ == "__main__":
     #print("the mean of these two values is: ", stats.mean([even_var,odd_var]))
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     #part5 
-    cipher= encrypt(sample_plaintext, key5)
-    test= find_len2(cipher, 6)
-    print(test)
+    #cipher= encrypt(sample_plaintext, key5)
+    #test= find_len2(cipher, 6)
+    #print(test)
     #two=find_len(cipher,2)
     #three=find_len(cipher,3)
     #four=find_len(cipher,4)
@@ -155,3 +165,27 @@ if __name__ == "__main__":
     #eleven= find_len(cipher,11)
     #twelve= find_len(cipher,12)
     #print(stats.mean([two,three,four,five, six, seven, eight, nine, ten, eleven, twelve]))
+    #test= [1,2,3,4,5,6,7,8,9,10,11,12,13]
+    #print(test[0::3])
+    #print(test[::3])
+    #print(test[1::3])
+    #print(test[2::3])
+    #print('2 ', find_len2(cipher,2))
+    #print('3 ', find_len2(cipher,3))
+    #print('4 ', find_len2(cipher,4))
+    #print('5 ', find_len2(cipher,5))
+    #print('6 ', find_len2(cipher,6))
+    #print('7 ', find_len2(cipher,7))
+    #print('8 ', find_len2(cipher,8))
+    #print('9 ', find_len2(cipher,9))
+    #print('10', find_len2(cipher,10))
+    #print('11', find_len2(cipher,11))
+    #print('12', find_len2(cipher,12))
+    #print('13', find_len2(cipher,13))
+    #find_len2 became find_len_helper
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #apply the same concept but in a loop and find the variance that is closest to the true value.
+    #print(find_len(cipher))
+
+    
+

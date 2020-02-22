552695dbf210056ef7ea1a917780757237e853b9
= <=>
2020-02-21 17:26:33 -0700

    injection Attack cleanup

--- p2/sql_2-src/injectionAttack.py
+++ p2/sql_2-src/injectionAttack.py
@@ -4,13 +4,11 @@ import re
 p = re.compile(b'\'=\'')
 
 found = False
-i = 0
 while (found == False):
 	password = secrets.token_hex(16) 
 	theHash = hashlib.md5(password.encode('utf-8')).digest()
 	if p.match(theHash) != None:
 		found = True
-	i = i + 1
 	
 print(password)
 print(theHash)

